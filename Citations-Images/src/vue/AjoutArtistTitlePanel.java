d7483d3f556112a994f5d0834005d9b0bee4ae12
adristrut <adrien.strutynski@gmail.com>
2013-06-18 11:57:41 +0200

    Mise en place projet sur RedDell

---
+++ Citations-Images/src/vue/AjoutArtistTitlePanel.java
@@ -0,0 +1,62 @@
+package vue;
+
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
+
+import observer.Observer;
+
+public class AjoutArtistTitlePanel extends SContainer implements Observer {
+
+	public AjoutArtistTitlePanel(Dimension dim) {
+		super(dim);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(String nomChanson, String img1Path, String img2Path,
+			String img3Path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restart(String word) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accueil() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(String nomChanson, ImageIcon imageIcon,
+			ImageIcon imageIcon2, ImageIcon imageIcon3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(String nomChanson, String img1Path, String img2Path,
+			String img3Path, int pts, int nbArtist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(String nomChanson, String nomChanteur, String img1Path,
+			String img2Path, String img3Path, int pts, int nbArtist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
